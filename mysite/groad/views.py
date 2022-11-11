054b6b38d2e0ed142ad657eeffa7720bf5e8e952
kooyunhyung <kooyh108@naver.com>
2022-11-11 20:13:33 +0900

    GROAD test23242

--- mysite/groad/views.py
+++ mysite/groad/views.py
@@ -204,11 +204,10 @@ class Groad_review_List(APIView):
         gr_grade = data.get('gr_grade')
         gr_date = data.get('gr_date')
         gr_content_image = data.get('gr_content_image')
-        gr_profile_image = data.get('gr_profile_image')
         gr_gu_seq_id = data.get('gr_gu_seq_id')
 
-        sql = f"""INSERT INTO groad_review(gr_name, gr_place, gr_content_text, gr_grade, gr_gu_seq_id,gr_date, gr_content_image, gr_profile_image)
-            value('{gr_name}','{gr_place}','{gr_content_text}','{gr_grade}','{gr_gu_seq_id}','{gr_date}','{gr_content_image}','{gr_profile_image}')"""
+        sql = f"""INSERT INTO groad_review(gr_name, gr_place, gr_content_text, gr_grade, gr_gu_seq_id,gr_date, gr_content_image)
+            value('{gr_name}','{gr_place}','{gr_content_text}','{gr_grade}','{gr_gu_seq_id}','{gr_date}','{gr_content_image}')"""
 
         try:
             cur = connection.cursor()
@@ -227,7 +226,7 @@ class Groad_review_List(APIView):
 class Groad_review_Detial(APIView):
     def get(self, request, fk):
 
-        sql = f"""SELECT gr_seq, gr_name, gr_place, gr_content_text, gr_grade, gr_gu_seq_id, gr_date, gr_content_image, gr_profile_image FROM groad_review INNER JOIN groad_user 
+        sql = f"""SELECT gr_seq, gr_name, gr_place, gr_content_text, gr_grade, gr_gu_seq_id, gr_date, gr_content_image FROM groad_review INNER JOIN groad_user 
         ON gr_gu_seq_id=gu_seq WHERE gr_gu_seq_id={fk}
         """
 
@@ -257,13 +256,11 @@ class Groad_review_Detial(APIView):
         gr_grade = data.get('gr_grade')
         gr_date = data.get('gr_date')
         gr_content_image = data.get('gr_content_image')
-        gr_profile_image = data.get('gr_profile_image')
         gr_gu_seq_id = data.get('gr_gu_seq_id')
 
         sql = f"""UPDATE groad_review SET 
         gr_name='{gr_name}',gr_place='{gr_place}', gr_content_text='{gr_content_text}', 
-        gr_grade='{gr_grade}',gr_gu_seq_id='{gr_gu_seq_id}',gr_date='{gr_date}',gr_content_image='{gr_content_image}',
-        gr_profile_image = '{gr_profile_image}'
+        gr_grade='{gr_grade}',gr_gu_seq_id='{gr_gu_seq_id}',gr_date='{gr_date}',gr_content_image='{gr_content_image}'
         WHERE gr_gu_seq_id={fk}"""
 
         try:
