4725946482f2023bcd95852392805b4be9597945
kooyunhyung <kooyh108@naver.com>
2023-05-17 18:15:41 +0900

    delete poll app and fix urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('groad/', include('groad.urls')),
     path('admin/', admin.site.urls),
 ]
